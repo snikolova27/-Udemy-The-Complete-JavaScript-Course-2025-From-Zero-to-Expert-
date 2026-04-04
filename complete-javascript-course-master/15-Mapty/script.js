e1ea9e20be74bfe818176c3197b480982ba05b53
snikolova27 <snikolova27@gmail.com>
2026-04-04 15:43:25 +0300

    add toast, delete workout, sort and delete all workouts, confirmation modals for deletion

--- complete-javascript-course-master/15-Mapty/script.js
+++ complete-javascript-course-master/15-Mapty/script.js
@@ -68,12 +68,65 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// Overview buttons
+const overviewButtonsContainer = document.querySelector('.overview__container');
+const deleteAllBtn = document.querySelector('#delete-all-workouts-btn');
+const sortBtn = document.querySelector('#sort-workouts-btn');
+
+// Delete all workouts modal
+const deleteAllModal = document.querySelector('#delete-all-workouts-modal');
+const closeModalBtn = document.querySelector(
+  '#delete-all-workouts-modal-close',
+);
+const cancelModalBtn = document.querySelector('#modal-cancel-btn');
+const confirmModalBtn = document.querySelector('#modal-confirm-btn');
+
+// Workout buttons
+const deleteWorkoutButton = document.querySelector('.delete__btn');
+const editWorkoutButton = document.querySelector('.edit__btn');
+
+// Delete single workout modal
+const deleteWorkoutModal = document.querySelector('#delete-workout-modal');
+const closeWorkoutModalBtn = document.querySelector(
+  '#delete-workout-modal-close',
+);
+const cancelWorkoutModalBtn = document.querySelector(
+  '#workout-modal-cancel-btn',
+);
+const confirmWorkoutModalBtn = document.querySelector(
+  '#workout-modal-confirm-btn',
+);
+
+// Success toast for adding a workout
+const successToast = document.querySelector('#success-toast');
+const successToastCloseBtn = document.querySelector('#success-toast-close-btn');
+
+// Success toast for deleting a workout
+const successDeletionToast = document.querySelector('#deletion-toast');
+const successDeletionToastCloseBtn = document.querySelector(
+  '#deletion-toast-close-btn',
+);
+
+// All deleted toast
+const allDeletedToast = document.querySelector('#all-deleted-toast');
+const allDeletedToastCloseBtn = document.querySelector(
+  '#all-deleted-toast-close-btn',
+);
+
+// Input error toast
+const inputErrorToast = document.querySelector('#input-error-toast');
+const inputErrorToastCloseBtn = document.querySelector(
+  '#input-error-toast-close-btn',
+);
+
 class App {
   #mapZoomLevel = 10;
   #map;
   #mapEvent;
   #workouts = [];
   #localStorageKey = 'workouts';
+  #areSortedByDistance = false;
+  #markers = {};
 
   constructor() {
     this._getPosition();
@@ -83,7 +136,53 @@ class App {
 
     inputType.addEventListener('change', this._toggleElevationField);
 
-    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+    containerWorkouts.addEventListener(
+      'click',
+      this.#handleClickOnWorkout.bind(this),
+    );
+
+    // Overview buttons
+    deleteAllBtn.addEventListener('click', this.#handleDeleteAll.bind(this));
+    sortBtn.addEventListener('click', this.#sortWorkouts.bind(this));
+
+    // Delete all workouts modal buttons
+    closeModalBtn.addEventListener('click', this.#closeDeleteAllModal);
+    cancelModalBtn.addEventListener('click', this.#closeDeleteAllModal);
+    confirmModalBtn.addEventListener(
+      'click',
+      this.#deleteAllWorkoutsConfirmHandler.bind(this),
+    );
+
+    // Delete workout modal buttons
+    closeWorkoutModalBtn.addEventListener(
+      'click',
+      this.#closeDeleteWorkoutModal,
+    );
+    cancelWorkoutModalBtn.addEventListener(
+      'click',
+      this.#closeDeleteWorkoutModal,
+    );
+    confirmWorkoutModalBtn.addEventListener(
+      'click',
+      this.#handleConfirmDeleteWorkout.bind(this),
+    );
+
+    // Toasts
+    successToastCloseBtn.addEventListener('click', this.#closeSuccessToast);
+
+    successDeletionToastCloseBtn.addEventListener(
+      'click',
+      this.#closeSuccessToast,
+    );
+    allDeletedToastCloseBtn.addEventListener(
+      'click',
+      this.#closeAllDeletedToast,
+    );
+
+    inputErrorToastCloseBtn.addEventListener(
+      'click',
+      this.#closeInputErrorToast,
+    );
   }
 
   _getPosition() {
@@ -114,13 +213,30 @@ class App {
     this.#workouts.forEach(w => this._renderWorkoutMarker(w));
   }
 
-  _moveToPopup(event) {
+  #handleClickOnWorkout(event) {
     const workoutEl = event.target.closest('.workout');
     if (!workoutEl) {
       return;
     }
 
     const workout = this.#workouts.find(w => w.id === workoutEl.dataset.id);
+
+    // If we have clicked on a button
+    const targetClassList = event.target.classList.value;
+    if (targetClassList.includes('workout__button')) {
+      if (targetClassList.includes('delete__btn')) {
+        this._handleDeleteWorkoutButton(workout);
+      } else if (targetClassList.includes('edit__btn')) {
+        console.log('edit');
+        
+      }
+    }
+    // Move to popup otherwise
+    else {
+      this._moveToPopup(workout);
+    }
+  }
+  _moveToPopup(workout) {
     this.#map.setView(workout.coords, this.#mapZoomLevel, {
       animate: true,
       pan: { duration: 1 },
@@ -145,6 +261,35 @@ class App {
     }, 1000);
   }
 
+  #showOverviewButtons() {
+    overviewButtonsContainer.classList.remove('hidden');
+  }
+
+  #hideOverviewButtons() {
+    overviewButtonsContainer.classList.add('hidden');
+  }
+
+  #closeDeleteAllModal() {
+    deleteAllModal.classList.add('hidden');
+  }
+
+  #deleteAllWorkoutsConfirmHandler() {
+    this.#closeDeleteAllModal();
+    this.#showAllDeletedToast();
+
+    setTimeout(() => {
+      this.reset();
+    }, 2000);
+  }
+
+  #handleDeleteAll() {
+    deleteAllModal.classList.remove('hidden');
+  }
+
+  #sortWorkouts() {
+    this.#renderWorkouts(!this.#areSortedByDistance);
+    this.#areSortedByDistance = !this.#areSortedByDistance;
+  }
   _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
@@ -160,7 +305,11 @@ class App {
 
   #handleInvalidInput() {
     this._clearAllInputs();
-    return alert('Inputs have to be positive numbers');
+    this.#showInputErrorToast();
+
+    setTimeout(() => {
+      this.#closeInputErrorToast();
+    }, 5000);
   }
 
   _newWorkout(e) {
@@ -210,13 +359,15 @@ class App {
 
     this._renderWorkoutMarker(workout);
     this._renderWorkout(workout);
+    this.#showSuccessToast();
     this._clearAllInputs();
     this._hideForm();
     this.#saveIntoLocalStorage();
+    this.#showOverviewButtons();
   }
 
   _renderWorkoutMarker(workout) {
-    L.marker(workout.coords)
+    const marker = L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -231,12 +382,14 @@ class App {
         `${workout.type === 'running' ? '🏃' : '🚴‍♀️'} ${workout.description}`,
       )
       .openPopup();
+
+    this.#markers[workout.id] = marker;
   }
 
   _renderWorkout(workout) {
     let html = `
         <li class="workout workout--${workout.type}" data-id=${workout.id}>
-                <h2 class="workout__title">${workout.description}</h2>
+               <h2 class="workout__title">${workout.description}</h2>
                 <div class="workout__details">
                   <span class="workout__icon">${workout.type === 'running' ? '🏃' : '🚴‍♀️'}</span>
                   <span class="workout__value">${workout.distance}</span>
@@ -274,15 +427,43 @@ class App {
           </div>`;
     }
 
+    html += `
+    <div class="workout__details ">
+    <button class="edit__btn workout__button" id>📝 Edit</button>
+    </div>
+    <div class="workout__details">
+    <button class="delete__btn workout__button">🗑️ Delete</button>
+    </div>`;
+
     html += `</li>`;
 
     form.insertAdjacentHTML('afterend', html);
   }
 
-  #renderWorkouts() {
-    this.#workouts.forEach(w => {
+  #renderWorkouts(sort = false) {
+    // Empty entire workout container
+    containerWorkouts.innerHTML = '';
+    containerWorkouts.appendChild(form);
+
+    const sorted = sort
+      ? this.#workouts
+          .slice()
+          .sort((a, b) =>
+            !this.#areSortedByDistance
+              ? a.distance - b.distance
+              : (b.distance = a.distance),
+          )
+      : this.#workouts;
+
+    sorted.forEach(w => {
       this._renderWorkout(w);
     });
+
+    if (this.#workouts.length > 0) {
+      this.#showOverviewButtons();
+    } else {
+      this.#hideOverviewButtons();
+    }
   }
 
   #saveIntoLocalStorage() {
@@ -303,6 +484,82 @@ class App {
   reset() {
     localStorage.removeItem(this.#localStorageKey);
     location.reload();
+  }
+
+  #openDeleteWorkoutModal(workoutId) {
+    deleteWorkoutModal.dataset.workoutId = workoutId;
+    deleteWorkoutModal.classList.remove('hidden');
+  }
+
+  #closeDeleteWorkoutModal() {
+    delete deleteWorkoutModal.dataset.workoutId;
+    deleteWorkoutModal.classList.add('hidden');
+  }
+
+  _handleDeleteWorkoutButton(workout) {
+    this.#openDeleteWorkoutModal(workout.id);
+  }
+
+  #handleConfirmDeleteWorkout(event) {
+    const workoutId = event.target.closest('#delete-workout-modal').dataset
+      .workoutId;
+    this.#deleteWorkout(workoutId);
+    this.#closeDeleteWorkoutModal();
+  }
+
+  #deleteWorkout(workoutId) {
+    // Delete from workouts
+    this.#workouts = this.#workouts.filter(w => w.id !== workoutId);
+
+    // Update rendered workouts
+    this.#renderWorkouts();
+
+    // Remove pin from map
+    this.#markers[workoutId].remove();
+    delete this.#markers[workoutId];
+
+    // Show toast
+    this.#showSuccessDeletionToast();
+
+    // Remove from local storage
+    this.#saveIntoLocalStorage();
+  }
+
+  #showSuccessToast() {
+    successToast.classList.remove('hidden');
+    setTimeout(() => {
+      this.#closeSuccessToast();
+    }, 3000);
+  }
+  #closeSuccessToast() {
+    successToast.classList.add('hidden');
+  }
+
+  #showSuccessDeletionToast() {
+    successDeletionToast.classList.remove('hidden');
+    setTimeout(() => {
+      this.#closeSuccessDeletionToast();
+    }, 3000);
+  }
+  #closeSuccessDeletionToast() {
+    successDeletionToast.classList.add('hidden');
+  }
+
+  #showAllDeletedToast() {
+    allDeletedToast.classList.remove('hidden');
+    setTimeout(() => {
+      this.#closeAllDeletedToast();
+    }, 3000);
+  }
+  #closeAllDeletedToast() {
+    allDeletedToast.classList.add('hidden');
+  }
+
+  #showInputErrorToast() {
+    inputErrorToast.classList.remove('hidden');
+  }
+  #closeInputErrorToast() {
+    inputErrorToast.classList.add('hidden');
   }
 }
 
