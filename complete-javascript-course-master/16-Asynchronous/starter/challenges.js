e1b50ac8966e6fe58c1040230798c0bc24188cfd
snikolova27 <snikolova27@gmail.com>
2026-04-05 13:49:00 +0300

    add fetch and xml requests

---
+++ complete-javascript-course-master/16-Asynchronous/starter/challenges.js
@@ -0,0 +1,113 @@
+'use strict';
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
+The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.508&longitude=13.381. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1_000_000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+    </article>
+    `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
+const handleResponse = (url, errorMessage = '') => {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`Something went wrong. ${errorMessage}.`);
+    }
+    return response.json();
+  });
+};
+
+const getCountryAndNeighbourDataWithErrorHandling = function (country) {
+  handleResponse(
+    `https://restcountries.com/v2/name/${country}`,
+    'Country not found',
+  )
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) throw new Error('No neighbour found.');
+
+      return handleResponse(
+        `https://restcountries.com/v2/alpha/${neighbour}`,
+        'Country not found',
+      );
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      renderError(`Something went wrong 💥: ${err.message} Try again.`);
+      console.error(`${err} 💥`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+const reverseGeocodeLocation = (lat, lng) => {
+  return handleResponse(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+    'Something went wrong during geocoding.',
+  )
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.countryName}`);
+      return data;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+const whereAmI = (lat, lng) => {
+  reverseGeocodeLocation(lat, lng)
+    .then(data => {
+      getCountryAndNeighbourDataWithErrorHandling(
+        data.countryName.toLowerCase(),
+      );
+    })
+    .catch(err => console.error(err));
+};
+
+whereAmI(52.508, 13.381);
